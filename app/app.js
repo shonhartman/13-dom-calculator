a24ef88576e4e7f140c35e784611fc8f38309cf7
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-04-05 23:04:28 -0500

    ...

--- app/app.js
+++ app/app.js
@@ -1,8 +1,50 @@
 
-#numberOne.addEventListener("click", function());
-#numberTwo.addEventListener("click", function());
-#addNumbers.addEventListener("click", function());
-#subtractNumbers.addEventListener("click", function());
-#divideNumbers.addEventListener("click", function());
-#multiplyNumbers.addEventListener("click", function());
-#answer.addEventListener("click", function());
+//TAKES TWO DIGITS
+
+let digits = document.querySelectorAll(".digit")
+
+Array.from(digits).forEach((digit) => {
+
+  digit.addEventListener("click", (e) => {
+
+    let textEl = document.createTextNode(e.target.innerHTML);
+    document.querySelector("#answer").appendChild(textEl);
+
+  })
+
+})
+
+//OPERATORS TO SCREEN
+
+document.querySelector("#addNumbers").addEventListener ("click", (e) => {
+  let plus = document.createTextNode("+");
+  document.querySelector("#answer").appendChild(plus);
+})
+
+document.querySelector("#subtractNumbers").addEventListener ("click", (e) => {
+  let subtract = document.createTextNode("-");
+  document.querySelector("#answer").appendChild(subtract);
+})
+
+document.querySelector("#divideNumbers").addEventListener ("click", (e) => {
+  let divide = document.createTextNode("/");
+  document.querySelector("#answer").appendChild(divide);
+})
+
+document.querySelector("#multiplyNumbers").addEventListener ("click", (e) => {
+  let multiply = document.createTextNode("*");
+  document.querySelector("#answer").appendChild(multiply);
+})
+
+//CLEAR
+document.querySelector(".clear").addEventListener ("click", (e) => {
+  document.querySelector("#answer").innerHTML = "";
+})
+
+//EQUALS
+
+document.querySelector("#equals").addEventListener ("click", (e) => {
+  let evaluate = document.querySelector("#answer").innerHTML;
+  document.querySelector("#answer").innerHTML = eval(evaluate);
+
+})
